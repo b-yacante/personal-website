d5256d6404d290af25c5fc76b5c9aca4ee936168
Braian Yacante <byacante@i-rent.net>
2024-10-17 21:53:43 -0300

    delete protect routes

--- app/game-dev/page.tsx
+++ app/game-dev/page.tsx
@@ -1,17 +1,12 @@
 'use client'
-
 import PresentationGamePage from '@/components/game-dev/presentation-game'
-import ProtectedRoute from '@/components/ProtectedRoute'
-const protectedRoutes = ['/game-dev']
 
 export default function GameDev() {
   return (
-    <ProtectedRoute protectedRoutes={protectedRoutes}>
-      <div className="grid min-w-full place-items-center gap-4 divide-y divide-slate-600">
-        <section id="game-presentation">
-          <PresentationGamePage />
-        </section>
-      </div>
-    </ProtectedRoute>
+    <div className="grid min-w-full place-items-center gap-4 divide-y divide-slate-600">
+      <section id="game-presentation">
+        <PresentationGamePage />
+      </section>
+    </div>
   )
 }
